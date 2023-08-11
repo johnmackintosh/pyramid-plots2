9c72d9bff921c54bc3f719cfc4a2d6f90a0c175a
John MacKintosh <johnmackintosh@users.noreply.github.com>
2023-08-11 14:58:13 +0100

    amend to CP_Name

--- R/phi_pop_pyramid.R
+++ R/phi_pop_pyramid.R
@@ -4,7 +4,6 @@ phi_pop_pyramid <- function(sourcedata = pyramid_tots,
                         hscpval,
                         xcol = pop_age_band,
                         ycol = pop_age_band_total,
-                        geography_col = CP_Name,
                         fill_col = Sex,
                         male_col = '#0391BF',
                         female_col ="grey70",
@@ -25,7 +24,7 @@ phi_pop_pyramid <- function(sourcedata = pyramid_tots,
 
 
   tempdf <- collapse::fsubset(sourcedata,
-                              {{geography_col}} ==  hscpval) %>%
+                              CP_Name ==  hscpval) %>%
     dplyr::mutate({{ycol}} := dplyr::case_when(
         {{fill_col}} == "Male" ~ {{ycol}} * -1,
         .default = {{ycol}}))
